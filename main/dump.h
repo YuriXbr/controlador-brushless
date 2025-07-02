fe0be1e7d9136e88f981bd7f40dc2896a356cfc7
Yuri A. dos Anjos <yurixbroficial@gmail.com>
2025-07-02 14:12:22 -0300

    Delete main directory

--- main/dump.h
+++
@@ -1,8 +0,0 @@
-#ifndef DUMP_H
-#define DUMP_H
-
-// Declaração das funções de dump para depuração
-void dumpAll();
-void emergencyDump(float currentAngle, int currentPWM, float currentPIDOutput);
-
-#endif // DUMP_H
--- main/gyroController.h
+++
@@ -1,9 +0,0 @@
-#ifndef GYROCONTROLLER_H
-#define GYROCONTROLLER_H
-
-#include <Arduino.h>
-
-void initGyro();
-float readGyroAngle();
-
-#endif
